ea2ef6bfd2bf525f2fc995698ef742a5b4b813b0
amal-nj <52827969+amal-nj@users.noreply.github.com>
2019-10-23 12:36:38 +0300

    first test

---
+++ index.js
@@ -0,0 +1,6 @@
+var uselessFunction=()=>{
+    console.log("Congrats! you've installed this useless package seccussfully!");
+    console.log("your life did not get any better!");
+}
+
+module.exports=uselessFunction;
